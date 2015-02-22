694f94544e48ca6d7fe38e716eae832bb44ecef4
xueyang <prodigious@163.com>
2015-02-22 23:44:05 +0800

    update RLS

--- src/regression/RLS.py
+++ src/regression/RLS.py
@@ -1,11 +1,19 @@
 __author__ = 'AaronXue'
 
-
+import numpy as np
 from src.base.base import *
 from src.regression.RegressionBase import RegressionBase
 
 
 class RLS(RegressionBase):
 
+    def __init__(self, sample_x, sample_y, k=1, lam=0.5):
+        super().__init__(sample_x, sample_y, k)
+        self._lam = lam
+
     def calculate(self):
-        pass
+
+        phi = feature_transformation(self._sample_x,self._k,get_matrix_col_num(self._sample_x))
+        self._theta = (phi*phi.T+np.identity(self._k)*self._lam).I*phi*self._sample_y
+        return self
+
