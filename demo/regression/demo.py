2a93494e778c669adfc76206c32c0a0402cf642a
xueyang <prodigious@163.com>
2015-02-22 23:44:21 +0800

    update demo

--- demo/regression/demo.py
+++ demo/regression/demo.py
@@ -2,8 +2,11 @@ __author__ = 'AaronXue'
 
 from src.base import base
 from src.regression.LS import LS
+from src.regression.RLS import RLS
+from src.regression.LASSO import LASSO
 
-def ls_regression_demo():
+def demo_read_data():
+
     sample_x_addr = "data/polydata_data_sampx.txt"
     sample_y_addr = "data/polydata_data_sampy.txt"
     poly_x_addr = "data/polydata_data_polyx.txt"
@@ -11,14 +14,35 @@ def ls_regression_demo():
     sample_x = base.read_matrix(sample_x_addr)
     sample_y = base.read_matrix(sample_y_addr)
     poly_x = base.read_matrix(poly_x_addr)
+    return sample_x, sample_y, poly_x
 
-    ls = LS(sample_x,sample_y,5)
-    ls.calculate().predict(poly_x)
-    base.plot_regression("least-square regression", ls)
+def ls_regression_demo():
+
+    (sample_x, sample_y, poly_x) = demo_read_data()  #read data
+    regression = LS(sample_x,sample_y,5)
+    regression.calculate().predict(poly_x)
+    base.plot_regression("least-square regression", regression)
+
+def rls_regression_demo():
+
+    (sample_x, sample_y, poly_x) = demo_read_data()  #read data
+    regression = RLS(sample_x,sample_y,5)
+    regression.calculate().predict(poly_x)
+    base.plot_regression("regularized least-square regression", regression)
+
+def lasso_regression_demo():
+
+    (sample_x, sample_y, poly_x) = demo_read_data()  #read data
+    regression = LASSO(sample_x,sample_y,5)
+    regression.calculate().predict(poly_x)
+    base.plot_regression("L1-regularized least-square regression", regression)
 
 
+#ls_regression_demo()
+#rls_regression_demo()
+lasso_regression_demo()
 
-ls_regression_demo()
+
 
 
 
