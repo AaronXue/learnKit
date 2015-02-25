f6e422da0390784db7124602d4da4c7f4db3588b
xueyang <prodigious@163.com>
2015-02-26 00:34:47 +0800

    update k_means

--- src/cluster/KMeans.py
+++ src/cluster/KMeans.py
@@ -3,89 +3,66 @@ __author__ = 'AaronXue'
 
 from src.cluster.ClusterBase import ClusterBase
 import numpy as np
-from src.math import norm
+import math
+from src.base.base import *
 
-D=4
-VAR=5
+CONVERGE = 0.001
 
 class KMeans(ClusterBase):
 
-    def StepOne(sampleA,sampleN,cluster_means,K):
-        Z=np.zeros((sampleN,K))
-        for i,valueX in enumerate(sampleA.getA()):
-            min=float("Inf")
-            for j,valueM in enumerate(cluster_means.getA()):
-                if norm.L2_norm(valueX,valueM)<min:
-                    min=norm.L2_norm(valueX,valueM)
-                    t=j
-            Z[i,t]=1
-        return cluster_means,Z
+    def __init__(self, sample, k, iter=0):
 
-    def StepTwo(sampleA,sampleN,cluster_means,Z,K):
-        for j in range(K):
-            cluster_means[j]=np.sum([Z[i][j]*sampleA[i] for i in range(sampleN)],axis=0)/sum([Z[i][j] for i in range(sampleN)])
-        return cluster_means
+        super().__init__(sample, k)
+        self._z = None
+        self._iter = iter
 
 
+    def step_one(self):
+
+        self._z = np.zeros((get_matrix_row_num(self._sample), self._k))
+        for i, sample_value in enumerate(self._sample):
+            min = float("inf")
+            t = None
+            for j, cluster_value in enumerate(self._cluster_means):
+                if np.linalg.norm(sample_value - cluster_value) < min:
+                    min = np.linalg.norm(sample_value - cluster_value)
+                    t = j
+            self._z[i,t] = 1
+
+    def step_two(self):
+        n = get_matrix_row_num(self._sample)
+        for j in range(self._k):
+            self._cluster_means[j] = np.sum([self._z[i][j]*self._sample[i] for i in range(n)], axis=0) / np.sum([self._z[i][j] for i in range(n)])
+
+    def cluster(self):
+
+        self._cluster_means = self._init_cluster_means()
+        prev_cluster_means = self._cluster_means
+        if self._iter == 0:
+            while True:
+                self.step_one()
+                self.step_two()
+                if np.linalg.norm(prev_cluster_means - self._cluster_means) < CONVERGE:
+                    break
+        else:
+            for i in range(self._iter):
+                self.step_one()
+                self.step_two()
+
+
+        return self._get_result()
+
+
+    def _get_result(self):
+
+        n = get_matrix_row_num(self._sample)
+        result = np.zeros(n)
+        for i in range(n):
+            for j in range(self._k):
+                if self._z[i][j] == 1:
+                    result[i] = j
+        return result.getA1()
 
 
 
 
-
-
-
-def CalcJ(sampleA,sampleN,cluster_means,Z,K,cov):
-    jsum=0
-    for i in range(sampleN):
-        for j in range(K):
-            jsum+=Z[i][j]*math.log(1/K*lib.base.MultiGaussian(sampleA[i],cluster_means[j],cov))
-    return jsum
-
-
-def calculate(sampleA,sampleN,K,iter):
-    cluster_means=lib.base.ClusterInit(sampleA,sampleN,K)
-    print("init")
-    cov=numpy.identity(D)*VAR
-    J0=None
-    J=None
-    '''
-    while True:
-        cluster_means,Z=StepOne(sampleA,sampleN,cluster_means,K)
-        cluster_means=StepTwo(sampleA,sampleN,cluster_means,Z,K)
-        J=CalcJ(sampleA,sampleN,cluster_means,Z,K,cov)
-        print(J)
-        if lib.base.ConvergeJ(J0,J):
-            break
-        J0=J
-    '''
-
-    for i in range(iter):
-        cluster_means,Z=StepOne(sampleA,sampleN,cluster_means,K)
-        cluster_means=StepTwo(sampleA,sampleN,cluster_means,Z,K)
-        J=CalcJ(sampleA,sampleN,cluster_means,Z,K,cov)
-        print(i)
-
-    rows=sampleA.shape[0]
-    result=numpy.zeros((1,rows))
-    '''
-    for j in range(K):
-        sampleCluster=[]
-        for i in range(sampleN):
-            if Z[i][j]==1:
-                t=sampleA.getA()[i]
-                sampleCluster.append(t)
-        samplePlot=numpy.array(sampleCluster)
-        plt.plot(samplePlot[:,0],samplePlot[:,1],"o",label="Cluster "+str(j))
-    plt.title("K means K="+str(K))
-    plt.legend(loc=3)
-    plt.show()
-
-    return J
-    '''
-    for j in range(K):
-        for i in range(sampleN):
-            if Z[i][j]==1:
-                result[0][i]=j
-
-
-    return result
