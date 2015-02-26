585dad736b0c5fbd0f5ab3e41bfcca3fc91d471e
xueyang <prodigious@163.com>
2015-02-26 14:26:04 +0800

    update cluster base class

--- src/cluster/ClusterBase.py
+++ src/cluster/ClusterBase.py
@@ -11,15 +11,25 @@ class ClusterBase(object):
 
     def __init__(self, sample, k):
 
-        self._sample = get_numpy_mat(sample)
+        self._sample = get_numpy_mat(sample).T
         self._k = k
         self._cluster_means = None
 
     def _init_cluster_means(self):
 
-        return np.mat([self._sample.getA()[i] for i in np.random.choice(get_matrix_col_num(self._sample), self._k)])
+        return np.mat([self._sample.getA()[i] for i in np.random.permutation(get_matrix_row_num(self._sample))[0:self._k]])
 
+    def get_k(self):
 
+        return self._k
+
+    def get_sample(self):
+
+        return self._sample.getA()
+
+    def get_result(self):
+
+        return self._result
 
 
 
