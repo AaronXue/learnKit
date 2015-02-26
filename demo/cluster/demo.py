c4058e1228fc6ab13eb6fdc3d8f75b8b2a5d0b8e
xueyang <prodigious@163.com>
2015-02-26 14:26:18 +0800

    update cluster demo

--- demo/cluster/demo.py
+++ demo/cluster/demo.py
@@ -2,11 +2,14 @@ __author__ = 'AaronXue'
 
 
 from src.base import base
-
+from src.cluster.KMeans import KMeans
 
 sample_addr = "data/cluster_data_dataA_X.txt"
 
 sample = base.read_matrix(sample_addr)
-sample = sample.T
+
+cluster = KMeans(sample, 3)
+result = cluster.cluster()
+base.plot_cluster("K Means", cluster, result)
 
 
