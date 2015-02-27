2ba9a844aebba498d0b92e0d5db5aa8667d7e6bf
xueyang <prodigious@163.com>
2015-02-28 00:43:17 +0800

    correct multivariate gaussian distribution

--- src/math/distribution.py
+++ src/math/distribution.py
@@ -3,9 +3,10 @@ __author__ = 'AaronXue'
 
 import math
 import numpy as np
+from src.base.base import *
 
-def multi_gaussian(x,mean,cov):
-    d=len(x.getA1())
-    t=(x-mean).T
-    tcov=np.mat(cov)
-    return float(1/(np.power(2*math.pi,d/2)*np.linalg.det(cov))*np.exp((-1)/2*t.T*tcov.I*t))
+def multi_gaussian(x, mean, cov):
+    d = get_matrix_col_num(x)
+    t = (x-mean).T
+    cov_mat = np.mat(cov)
+    return float(1/(np.power(2*math.pi,d/2)*np.power(np.linalg.det(cov),1/2))*np.exp((-1)/2*t.T*cov_mat.I*t))
