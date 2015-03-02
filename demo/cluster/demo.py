ce03ec155fc236cc3fda8694e1847b82d0589d87
xueyang <prodigious@163.com>
2015-03-03 00:03:02 +0800

    update cluster demo

--- demo/cluster/demo.py
+++ demo/cluster/demo.py
@@ -9,8 +9,14 @@ sample_addr = "data/cluster_data_dataA_X.txt"
 
 sample = base.read_matrix(sample_addr)
 
-cluster = EM(sample, 4, 5)
+cluster = EM(sample, 4, 15)
 result = cluster.cluster()
-print(result)
+plot_list = [0]*len(result)
+for i in range(len(result)):
+    max_num = max(result[i])
+    plot_list[i] = result[i].index(max_num)
+print(plot_list)
+base.plot_cluster("K-Means cluster", cluster, plot_list)
+
 
 
